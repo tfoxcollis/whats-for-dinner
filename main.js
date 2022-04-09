0c1021e5dc63df837059cdc740e53a258d8f277c
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-04-09 12:35:39 -0700

    Create data file to hold arrays

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-var radioSide = document.querySelector("#side");
-var radioMainDish = document.querySelector("#main");
-var radioDessert = document.querySelector("#dessert");
-var radioEntireDish = document.querySelector("#entire-dish");
 var letsCookButton = document.querySelector(".button-submit");
 var cookPot = document.querySelector("#cookpot");
 var dishDisplay = document.querySelector(".dish-display");
@@ -10,71 +6,22 @@ var displayButton = document.querySelector(".button");
 var clearButton = document.querySelector(".clear-button");
 var findAllRadio = document.querySelectorAll(".radiodish");
 var randomRecipe
-var sides = ["Miso Glazed Carrots",
-"Coleslaw",
-"Garden Salad",
-"Crispy Potatoes",
-"Sweet Potato Tots",
-"Coconut Rice",
-"Caeser Salad",
-"Shrimp Summer Rolls",
-"Garlic Butter Mushrooms",
-"Hush Puppies"]
-
-var mainDish = ["Spaghetti and Meatballs",
-"Pineapple Chicken",
-"Shakshuka",
-"Thai Yellow Curry",
-"Bibimbap",
-"Chicken Parmesean",
-"Butternut Squash Soup",
-"BBQ Chicken Burgers",
-"Ramen",
-"Empanadas",
-"Chicken Fried Rice",
-"Sheet Pan Fajitas",
-"Margarita Pizza",
-]
-
-var dessert = ["Apple Pie",
-"Lemon Meringue Pie",
-"Black Forest Cake",
-"Banana Bread",
-"Peach Cobbler",
-"Cheesecake",
-"Funfetti Cake",
-"Baklava",
-"Flan",
-"Macarons",
-"Macaroons",
-"Chocolate Cupcakes",
-"Pavlova",
-"Pumpkin Pie",
-"Key Lime Pie",
-"Tart Tatin",
-"Croissants",
-"Eclairs"]
 var randomSide = sides[getRandomIndex(sides)];
 var randomMainDish = mainDish[getRandomIndex(mainDish)];
 var randomDessert = dessert[getRandomIndex(dessert)];
+
+
 letsCookButton.addEventListener("click", () => {
   event.preventDefault()
   var checkedItem = findCheckedItem();
+  
   setRandomRecipe(checkedItem);
   toggleCookPot();
-
-  if(radioEntireDish.checked === true){
-    cookPot.style.display = "none";
-    dishName.innerHTML = `${randomSide}, ${randomMainDish}, ${randomDessert}`
-    dishDisplay.style.display = "flex";
-  }
-  displayButton.style.display = "flex";
 })
 
 clearButton.addEventListener("click", () => {
   window.location.reload();
 })
-
 
 
 // Functions
@@ -91,17 +38,17 @@ function toggleCookPot() {
   cookPot.style.display = "none";
   dishName.innerHTML = randomRecipe;
   dishDisplay.style.display = "flex";
+  displayButton.style.display = "flex";
 }
 
 function setRandomRecipe(checkedItem) {
-  var randomRecipe = randomSide;
-  var randomRecipe = randomMainDish;
-  var randomRecipe = randomDessert;
   if(checkedItem.value === "sides") {
-    var randomRecipe = randomSide;
+    randomRecipe = randomSide;
   }else if(checkedItem.value === "mainDish") {
-    var randomRecipe = randomMainDish;
-  }else if(checkItem.value === "dessert") {
-    var randomRecipe = randomDessert;
+    randomRecipe = randomMainDish;
+  }else if(checkedItem.value === "dessert") {
+    randomRecipe = randomDessert;
+  }else if(checkedItem.value === "entire-dish") {
+    randomRecipe = `${randomSide}, ${randomMainDish}, ${randomDessert}`
   }
 }
